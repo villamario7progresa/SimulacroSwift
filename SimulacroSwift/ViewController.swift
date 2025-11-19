337add90b73360d7fd2e87144add8c68fdf0814f
alumno3 <villamario7@digitechfp.com>
2025-11-19 15:46:50 +0100

    Initial Commit

---
+++ SimulacroSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimulacroSwift
+//
+//  Created by alumno3 on 19/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
